3fdd7eeb641df2b73530b600201c6ea387c7d922
Anurag <anurag.khandelwal@primussoft.com>
2020-08-23 01:24:01 +0530

    Created Trader component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
+import Trader from './components/trader/Trader';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button>HAHAHHA </button>
+      <Button variant='danger'>Primary</Button>
+      <div className='d-flex'>
+        <Trader />
+        <Trader />
+      </div>
     </div>
   );
 }
